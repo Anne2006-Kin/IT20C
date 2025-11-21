125bc05fdee90dcb2350215487a45bc6c52b0464
Anne-Jane Kinoyog <kinoyogannejane@gmail.com>
2025-11-20 17:42:47 -1100

    Added For-loop

---
+++ for-loop.js
@@ -0,0 +1,19 @@
+//for loop
+
+/*for (i = 1; 1<=5; i++){
+    //console.log(i);
+    output += i + " ";
+}
+
+//console.log (output);*/
+
+//arrays
+output = " ";
+arr [1,2,3];
+console.log(arr.length);
+
+for (index =0; index <arr.length; index++){
+    //console.log(arr[i]);
+    output +=arr[index] + " ";
+}
+console.log(output);
