cecc0ede8a5ec7575b3aa278c53c5ddbb5667068
Anne-Jane Kinoyog <kinoyogannejane@gmail.com>
2025-11-20 17:46:55 -1100

    Added while-loop

---
+++ while-loop.js
@@ -0,0 +1,23 @@
+/*i = 5;
+
+while (i<=5){
+    console.log(i);
+    i--;
+}
+
+console.log("Ascending");
+x=1;
+while(x <= 5){
+    console.log(x);
+    x++;
+}*/
+
+arr = [4,2,3];
+output = " ";
+index = 0;
+
+while(index <= arr.lenth-1){
+    output += arr[index] + " ";
+    index++;
+}
+console.log(output);
