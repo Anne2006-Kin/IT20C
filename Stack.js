f7b3e4f546d5b270b72c67e39c04567c6f6628fa
Anne-Jane Kinoyog <kinoyogannejane@gmail.com>
2025-11-24 19:13:00 -1100

    Added pop and size method

--- Stack.js
+++ Stack.js
@@ -15,10 +15,27 @@ peek (){
         if (this.is_empty())return null;
         return this.data[this.topIndex];
     }
+    pop (){
+        if (this.is_empty()) return null;
+
+        let value = this.data[this.topIndex];
+        delete this.data[this.topIndex];
+        this.topIndex--;
+        return value;
+    }
+    size (){
+        return this.topIndex + 1;
+    }
 let stack1=new Stack();
 stack1.push(2);
 stack1.traverse();
 stack1.push(3);
 stack1.traverse();
 stack1.push(1);
+stack1.traverse();
+//console.log("Top element is: " + stack1.peek());
+console.log("Top element of the stack is: " + stack1.is_empty());
+stack1.pop();
+console.log("Top element " + stack1.pop()+ " is popped the new element" +stack1.peek());
+console.log("New size of the stack is: " + stack1.size());
 stack1.traverse();
